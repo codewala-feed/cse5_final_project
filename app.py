0797b52d646d6ce5bc9aad4cc9f2ff4e909963fe
nikhil <nikhilpokuri143@gmail.com>
2024-11-11 15:54:14 +0530

    project day3

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
+from pymongo import MongoClient
 
 app = Flask(__name__)
+host = "ocdb.app"
+port = 5050
+database = "db_42xt2ftzx"
+username = "user_42xt2ftzx"
+password = "p42xt2ftzx"
+
+connection_string = f"mongodb://{username}:{password}@{host}:{port}/{database}"
+my_connection = MongoClient(connection_string)
+my_db = my_connection[database]
+callme = my_db["callme"]
+students = my_db["students"]
 
 @app.route("/", methods=["GET"])
 def homepage():
@@ -11,24 +23,59 @@ def admission():
     return render_template("admissions.html")
 
 @app.route("/callme", methods=["POST", "GET"])
-def callme():
-    return "Call REquested..!!"
+def callme_details():
+    name = request.form["name"]
+    email = request.form["email"]
+    number = request.form["number"]
+    course = request.form["course"]
+    callme.insert_one({
+        "name":name, "email":email, "number":number, "course":course
+    })
+    return redirect("/")
 
-@app.route("/register", methods=["GET"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        _id = request.form["id"]
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        percentage = request.form["percentage"]
+        rank = request.form["rank"]
+        course = request.form["course"]
+        address = request.form["address"]
+
+        students.insert_one({
+            "id":_id, "name":name, "email":email, 
+            "phone":phone, "percentage":percentage, 
+            "rank":rank, "course":course, "address":address
+        })
+
+        return redirect("/register")
+    else:
+        return render_template("register.html")
 
 @app.route("/view", methods=["GET"])
 def view():
     return render_template("view.html")
 
-@app.route("/update", methods=["GET"])
+@app.route("/update", methods=["GET", "POST"])
 def update():
-    return render_template("update.html")
+    if request.method == "POST":
+       _id = request.form["id"]
+       new_field = request.form["new_field"]
+       new_value = request.form["new_value"]
+       return "Data Updated "
+    else:
+        return render_template("update.html")
 
-@app.route("/delete", methods=["GET"])
+@app.route("/delete", methods=["GET", "POST"])
 def delete():
-    return render_template("delete.html")
+    if request.method == "POST":
+        _id = request.form["id"]
+        return "Data Deleted"
+    else:
+        return render_template("delete.html")
 
 @app.route("/campus_tour", methods=["GET"])
 def campus_tour():
